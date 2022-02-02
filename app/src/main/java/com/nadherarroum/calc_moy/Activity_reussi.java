0d9b8a8023538de40af8295d701ddfb69746c171
Nadher Arroum <45337521+nadherarroum@users.noreply.github.com>
2022-02-02 02:04:37 +0100

    Design REUSSI activity + passing result throw

--- app/src/main/java/com/nadherarroum/calc_moy/Activity_reussi.java
+++ app/src/main/java/com/nadherarroum/calc_moy/Activity_reussi.java
@@ -2,13 +2,23 @@ package com.nadherarroum.calc_moy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity_reussi extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reussi);
+
+        textView = findViewById(R.id.result_moy_view);
+
+        String moy = getIntent().getStringExtra("keymoy");
+
+        textView.setText(moy);
     }
 }
