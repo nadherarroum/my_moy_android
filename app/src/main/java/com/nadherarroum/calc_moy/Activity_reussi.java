834aeb6afaa4df64a0d1825db348f915a916dffa
Nadher Arroum <45337521+nadherarroum@users.noreply.github.com>
2022-02-08 14:09:54 +0100

    Send result to  SMS app

--- app/src/main/java/com/nadherarroum/calc_moy/Activity_reussi.java
+++ app/src/main/java/com/nadherarroum/calc_moy/Activity_reussi.java
@@ -2,23 +2,42 @@ package com.nadherarroum.calc_moy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Activity_reussi extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView textView,statictext;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reussi);
 
+        button = findViewById(R.id.send_sms_btn);
+        statictext = findViewById(R.id.static_txt_view);
         textView = findViewById(R.id.result_moy_view);
 
         String moy = getIntent().getStringExtra("keymoy");
-
         textView.setText(moy);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSMSapp();
+            }
+        });
+    }
+
+    private void openSMSapp() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","55840111",null));
+        intent.putExtra("sms_body",statictext.getText().toString()+textView.getText().toString());
+        startActivity(intent);
     }
 }
